86cd2812bb24bf2c7a68587bef5f84674fb9697f
Peter Cwik <pjc1246@gmail.com>
2022-03-06 17:36:38 -0600

    finished assign 4

---
+++ hw3/proxyCreate.java
@@ -0,0 +1,7 @@
+public class proxyCreate {
+  public boolean isAtEnd(hw3.flight.Flight flight){
+    System.out.Println("A flight is being created");
+  }
+
+
+}
---
+++ hw3/proxyGet.java
@@ -0,0 +1,7 @@
+public class proxyGet {
+import F
+  public boolean isAtEnd(hw3.flight.Flight flight){
+    System.out.Println("Flight number: " + flightNumber + " is being retrieved.");
+  }
+
+}
